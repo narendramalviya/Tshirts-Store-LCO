36377c4a2c1e1cd7a3eca6f8e62fb0d42e51e993
Narendra Malviya <narendramalviya5@gmail.com>
2020-05-08 20:24:33 +0530

    adding create category component in admin dashboard

--- FRONTEND/src/Routes.js
+++ FRONTEND/src/Routes.js
@@ -3,17 +3,37 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./core/Home";
 import Signup from "./user/Signup";
 import Signin from "./user/Signin";
+import PrivateRoute from "./auth/helper/PrivateRoutes";
+import AdminRoute from "./auth/helper/AdminRoutes";
+import UserDashBoard from "./user/UserDashBoard";
+import AdminDashBoard from "./user/AdminDashBoard";
+import AddCategory from "./admin/AddCategory";
 
 const Routes = () => {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/signup" exact component={Signup} />
-        <Route path="/signin" exact component={Signin} />
-      </Switch>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route path="/" exact component={Home} />
+				<Route path="/signup" exact component={Signup} />
+				<Route path="/signin" exact component={Signin} />
+				<PrivateRoute
+					path="/user/dashboard"
+					exact
+					component={UserDashBoard}
+				/>
+				<AdminRoute
+					path="/admin/dashboard"
+					exact
+					component={AdminDashBoard}
+				/>
+				<AdminRoute
+					path="/admin/create/category"
+					exact
+					component={AddCategory}
+				/>
+			</Switch>
+		</BrowserRouter>
+	);
 };
 
 export default Routes;
